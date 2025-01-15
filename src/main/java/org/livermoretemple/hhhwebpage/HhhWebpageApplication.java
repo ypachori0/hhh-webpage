2f24ed1fdf43d2047b71d2918b98625ecb3103be
ypachori0 <ypachori0@gmail.com>
2025-01-15 10:52:00 -0800

    initial commit

---
+++ src/main/java/org/livermoretemple/hhhwebpage/HhhWebpageApplication.java
@@ -0,0 +1,13 @@
+package org.livermoretemple.hhhwebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhhWebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhhWebpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/livermoretemple/hhhwebpage/HhhWebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.livermoretemple.hhhwebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhhWebpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
